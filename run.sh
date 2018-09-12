1c53c9ad72244f35f4ab088f1f2a2a827e57f0e0
Vamsi Ramakrishnan <vamsiramakrishnan@gmail.com>
2018-09-12 17:34:23 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
-kubectl="$WERCKER_STEP_ROOT/helm-kubectl"
+helm="$WERCKER_STEP_ROOT/helm-kubectl"
 
 main() {
   display_version
 
-  if [ -z "$WERCKER_KUBECTL_COMMAND" ]; then
-    fail "wercker-kubectl: command argument cannot be empty"
-  fi
 
   if [ -z "$WERCKER_HELM_COMMAND" ]; then
     fail "wercker-helm: command argument cannot be empty"
   fi
   
-  kubectl_cmd="$WERCKER_KUBECTL_COMMAND"
+  helm_cmd="$WERCKER_HELM_COMMAND"
  
 
-  # Global args
   #global_args
   global_args=
   raw_global_args="$WERCKER_KUBECTL_RAW_GLOBAL_ARGS"
@@ -57,129 +53,8 @@ main() {
   if [ -n "$WERCKER_KUBECTL_CLIENT_KEY" ]; then
     global_args="$global_args --client-key=\"$WERCKER_KUBECTL_CLIENT_KEY\""
   fi
-
-  # Command specific flags
-  args=
-  raw_args="$WERCKER_KUBECTL_RAW_ARGS"
-
-  # file
-  if [ -n "$WERCKER_KUBECTL_FILE" ]; then
-    args="$args --file=\"$WERCKER_KUBECTL_FILE\""
-  fi
-
-  # output
-  if [ -n "$WERCKER_KUBECTL_OUTPUT" ]; then
-    args="$args --output=\"$WERCKER_KUBECTL_OUTPUT\""
-  fi
-
-  # template
-  if [ -n "$WERCKER_KUBECTL_TEMPLATE" ]; then
-    args="$args --template=\"$WERCKER_KUBECTL_TEMPLATE\""
-  fi
-
-  # patch
-  if [ -n "$WERCKER_KUBECTL_PATCH" ]; then
-    args="$args --patch=\"$WERCKER_KUBECTL_PATCH\""
-  fi
-
-  # interactive
-  if [ -n "$WERCKER_KUBECTL_INTERACTIVE" ]; then
-    args="$args --interactive=\"$WERCKER_KUBECTL_INTERACTIVE\""
-  fi
-
-  # image
-  if [ -n "$WERCKER_KUBECTL_IMAGE" ]; then
-    args="$args --image=\"$WERCKER_KUBECTL_IMAGE\""
-  fi
-
-  # timeout
-  if [ -n "$WERCKER_KUBECTL_TIMEOUT" ]; then
-    args="$args --timeout=\"$WERCKER_KUBECTL_TIMEOUT\""
-  fi
-
-  # update-period
-  if [ -n "$WERCKER_KUBECTL_UPDATE_PERIOD" ]; then
-    args="$args --update-period=\"$WERCKER_KUBECTL_UPDATE_PERIOD\""
-  fi
-
-  # deployment-label-key
-  if [ -n "$WERCKER_KUBECTL_DEPLOYMENT_LABEL_KEY" ]; then
-    args="$args --deployment-label-key=\"$WERCKER_KUBECTL_DEPLOYMENT_LABEL_KEY\""
-  fi
-
-  # poll-interval
-  if [ -n "$WERCKER_KUBECTL_POLL_INTERVAL" ]; then
-    args="$args --poll-interval=\"$WERCKER_KUBECTL_POLL_INTERVAL\""
-  fi
-
-  # rollback
-  if [ -n "$WERCKER_KUBECTL_ROLLBACK" ]; then
-    args="$args --rollback=\"$WERCKER_KUBECTL_ROLLBACK\""
-  fi
-
-  # replicas
-  if [ -n "$WERCKER_KUBECTL_REPLICAS" ]; then
-    args="$args --replicas=\"$WERCKER_KUBECTL_REPLICAS\""
-  fi
-
-  # current-replicas
-  if [ -n "$WERCKER_KUBECTL_CURRENT_REPLICAS" ]; then
-    args="$args --current-replicas=\"$WERCKER_KUBECTL_CURRENT_REPLICAS\""
-  fi
-
-  # resource-version
-  if [ -n "$WERCKER_KUBECTL_RESOURCE_VERSION" ]; then
-    args="$args --resource-version=\"$WERCKER_KUBECTL_RESOURCE_VERSION\""
-  fi
-
-  # pod
-  if [ -n "$WERCKER_KUBECTL_POD" ]; then
-    args="$args --pod=\"$WERCKER_KUBECTL_POD\""
-  fi
-
-  # stdin
-  if [ -n "$WERCKER_KUBECTL_STDIN" ]; then
-    args="$args --stdin=\"$WERCKER_KUBECTL_STDIN\""
-  fi
-
-  # tty
-  if [ -n "$WERCKER_KUBECTL_TTY" ]; then
-    args="$args --tty=\"$WERCKER_KUBECTL_TTY\""
-  fi
-
-  # grace-period
-  if [ -n "$WERCKER_KUBECTL_GRACE_PERIOD" ]; then
-    args="$args --grace-period=\"$WERCKER_KUBECTL_GRACE_PERIOD\""
-  fi
-
-  # selector
-  if [ -n "$WERCKER_KUBECTL_SELECTOR" ]; then
-    args="$args --selector=\"$WERCKER_KUBECTL_SELECTOR\""
-  fi
-
-  # all
-  if [ -n "$WERCKER_KUBECTL_ALL" ]; then
-    args="$args --all=\"$WERCKER_KUBECTL_ALL\""
-  fi
-
-  # overwrite
-  if [ -n "$WERCKER_KUBECTL_OVERWRITE" ]; then
-    args="$args --overwrite=\"$WERCKER_KUBECTL_OVERWRITE\""
-  fi
-
-
-  info "Running kubctl command"
-  if [ "$WERCKER_KUBECTL_DEBUG" = "true" ]; then
-    info "kubectl $global_args $raw_global_args $kubectl_cmd $args $raw_args"
-  fi
-
-  eval "$kubectl" "$global_args" "$raw_global_args" "$kubectl_cmd" "$args" "$raw_args"
-}
-
-display_kubectl_version() {
-  info "Running kubectl version:"
-  "$kubectl" version --client
-  echo ""
+  
+  eval "$helm" "$raw_global_args" "$cmd" "$args" "$raw_args"
 }
 
 display_helm_version() {
