0b5a50791a21d557696d6f5dcd00e1e52399d0e4
vamsi.ramakrishnan <vamsiramakrishnan@gmail.com>
2018-09-13 20:59:41 +0530

    update step version

--- run.sh
+++ run.sh
@@ -175,10 +175,11 @@ main() {
   if [ "$WERCKER_HELM_KUBECTL_DEBUG" = "true" ]; then
     info "kubectl $global_args $raw_global_args $cmd $args $raw_args"
   fi
-  
+
   cat "$WERCKER_STEP_ROOT"/config | while read line; do echo $(eval echo `echo $line`); done > "$WERCKER_STEP_ROOT"/config_modified
+  cp "$WERCKER_STEP_ROOT"/config_modified $HOME/.kube/config
   info "Initializing Helm"
-  "$helm" init --service-account tiller --kubeconfig "$WERCKER_STEP_ROOT"/config_modified
+  "$helm" init --service-account tiller
   info "Executing Helm Command"
   eval "$helm" "$helm_cmd"
   
