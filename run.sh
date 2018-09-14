a972a20b9ec60b39aef3e693118669ad69956ab0
Vamsi Ramakrishnan <vamsiramakrishnan@gmail.com>
2018-09-14 16:24:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,167 +11,59 @@ main() {
 #   fi
 
   cmd="cluster-info"
-  helm_cmd="$WERCKER_HELM_KUBECTL_COMMAND"
+  helm_cmd="$WERCKER_HELM_COMMAND"
  
+  helm_args=
+  # client only
+  if [ -n "$WERCKER_HELM_CLIENT_ONLY" ]; then
+    helm_args ="$helm_args --client-only"
+  fi
+  
   
   # Global args
   #global_args
   global_args=
-  raw_global_args="$WERCKER_HELM_KUBECTL_RAW_GLOBAL_ARGS"
+  raw_global_args="$WERCKER_HELM_RAW_GLOBAL_ARGS"
 
   # token
-  if [ -n "$WERCKER_HELM_KUBECTL_TOKEN" ]; then
-    global_args="$global_args --token=\"$WERCKER_HELM_KUBECTL_TOKEN\""
+  if [ -n "$WERCKER_HELM_TOKEN" ]; then
+    global_args="$global_args --token=\"$WERCKER_HELM_TOKEN\""
   fi
 
   # username
-  if [ -n "$WERCKER_HELM_KUBECTL_USERNAME" ]; then
-    global_args="$global_args --username=\"$WERCKER_HELM_KUBECTL_USERNAME\""
+  if [ -n "$WERCKER_HELM_USERNAME" ]; then
+    global_args="$global_args --username=\"$WERCKER_HELM_USERNAME\""
   fi
 
   # password
-  if [ -n "$WERCKER_HELM_KUBECTL_PASSWORD" ]; then
-    global_args="$global_args --password=\"$WERCKER_HELM_KUBECTL_PASSWORD\""
+  if [ -n "$WERCKER_HELM_PASSWORD" ]; then
+    global_args="$global_args --password=\"$WERCKER_HELM_PASSWORD\""
   fi
 
   # server
-  if [ -n "$WERCKER_HELM_KUBECTL_SERVER" ]; then
-    global_args="$global_args --server=\"$WERCKER_HELM_KUBECTL_SERVER\""
+  if [ -n "$WERCKER_HELM_SERVER" ]; then
+    global_args="$global_args --server=\"$WERCKER_HELM_SERVER\""
   fi
 
   # insecure-skip-tls-verify
-  if [ -n "$WERCKER_HELM_KUBECTL_INSECURE_SKIP_TLS_VERIFY" ]; then
-    global_args="$global_args --insecure-skip-tls-verify=\"$WERCKER_HELM_KUBECTL_INSECURE_SKIP_TLS_VERIFY\""
+  if [ -n "$WERCKER_HELM_INSECURE_SKIP_TLS_VERIFY" ]; then
+    global_args="$global_args --insecure-skip-tls-verify=\"$WERCKER_HELM_INSECURE_SKIP_TLS_VERIFY\""
   fi
     # certificate-authority
-  if [ -n "$WERCKER_HELM_KUBECTL_CERTIFICATE_AUTHORITY" ]; then
-    global_args="$global_args --certificate-authority=\"$WERCKER_HELM_KUBECTL_CERTIFICATE_AUTHORITY\""
+  if [ -n "$WERCKER_HELM_CERTIFICATE_AUTHORITY" ]; then
+    global_args="$global_args --certificate-authority=\"$WERCKER_HELM_CERTIFICATE_AUTHORITY\""
   fi
     # client-certificate
-  if [ -n "$WERCKER_HELM_KUBECTL_CLIENT_CERTIFICATE" ]; then
-    global_args="$global_args --client-certificate=\"$WERCKER_HELM_KUBECTL_CLIENT_CERTIFICATE\""
+  if [ -n "$WERCKER_HELM_CLIENT_CERTIFICATE" ]; then
+    global_args="$global_args --client-certificate=\"$WERCKER_HELM_CLIENT_CERTIFICATE\""
   fi
     # client-key
-  if [ -n "$WERCKER_HELM_KUBECTL_CLIENT_KEY" ]; then
-    global_args="$global_args --client-key=\"$WERCKER_HELM_KUBECTL_CLIENT_KEY\""
-  fi
-
-  # Command specific flags
-  args=
-  raw_args="$WERCKER_HELM_KUBECTL_RAW_ARGS"
-
-  # file
-  if [ -n "$WERCKER_HELM_KUBECTL_FILE" ]; then
-    args="$args --file=\"$WERCKER_HELM_KUBECTL_FILE\""
-  fi
-
-  # output
-  if [ -n "$WERCKER_HELM_KUBECTL_OUTPUT" ]; then
-    args="$args --output=\"$WERCKER_HELM_KUBECTL_OUTPUT\""
-  fi
-
-  # template
-  if [ -n "$WERCKER_HELM_KUBECTL_TEMPLATE" ]; then
-    args="$args --template=\"$WERCKER_HELM_KUBECTL_TEMPLATE\""
-  fi
-
-  # patch
-  if [ -n "$WERCKER_HELM_KUBECTL_PATCH" ]; then
-    args="$args --patch=\"$WERCKER_HELM_KUBECTL_PATCH\""
-  fi
-
-  # interactive
-  if [ -n "$WERCKER_HELM_KUBECTL_INTERACTIVE" ]; then
-    args="$args --interactive=\"$WERCKER_HELM_KUBECTL_INTERACTIVE\""
-  fi
-
-  # image
-  if [ -n "$WERCKER_HELM_KUBECTL_IMAGE" ]; then
-    args="$args --image=\"$WERCKER_HELM_KUBECTL_IMAGE\""
-  fi
-
-  # timeout
-  if [ -n "$WERCKER_HELM_KUBECTL_TIMEOUT" ]; then
-    args="$args --timeout=\"$WERCKER_HELM_KUBECTL_TIMEOUT\""
-  fi
-
-  # update-period
-  if [ -n "$WERCKER_HELM_KUBECTL_UPDATE_PERIOD" ]; then
-    args="$args --update-period=\"$WERCKER_HELM_KUBECTL_UPDATE_PERIOD\""
-  fi
-
-  # deployment-label-key
-  if [ -n "$WERCKER_HELM_KUBECTL_DEPLOYMENT_LABEL_KEY" ]; then
-    args="$args --deployment-label-key=\"$WERCKER_HELM_KUBECTL_DEPLOYMENT_LABEL_KEY\""
-  fi
-
-  # poll-interval
-  if [ -n "$WERCKER_HELM_KUBECTL_POLL_INTERVAL" ]; then
-    args="$args --poll-interval=\"$WERCKER_HELM_KUBECTL_POLL_INTERVAL\""
-  fi
-
-  # rollback
-  if [ -n "$WERCKER_HELM_KUBECTL_ROLLBACK" ]; then
-    args="$args --rollback=\"$WERCKER_HELM_KUBECTL_ROLLBACK\""
-  fi
-
-  # replicas
-  if [ -n "$WERCKER_HELM_KUBECTL_REPLICAS" ]; then
-    args="$args --replicas=\"$WERCKER_HELM_KUBECTL_REPLICAS\""
-  fi
-
-  # current-replicas
-  if [ -n "$WERCKER_HELM_KUBECTL_CURRENT_REPLICAS" ]; then
-    args="$args --current-replicas=\"$WERCKER_HELM_KUBECTL_CURRENT_REPLICAS\""
-  fi
-
-  # resource-version
-  if [ -n "$WERCKER_HELM_KUBECTL_RESOURCE_VERSION" ]; then
-    args="$args --resource-version=\"$WERCKER_HELM_KUBECTL_RESOURCE_VERSION\""
-  fi
-
-  # pod
-  if [ -n "$WERCKER_HELM_KUBECTL_POD" ]; then
-    args="$args --pod=\"$WERCKER_HELM_KUBECTL_POD\""
-  fi
-
-  # stdin
-  if [ -n "$WERCKER_HELM_KUBECTL_STDIN" ]; then
-    args="$args --stdin=\"$WERCKER_HELM_KUBECTL_STDIN\""
-  fi
-
-  # tty
-  if [ -n "$WERCKER_HELM_KUBECTL_TTY" ]; then
-    args="$args --tty=\"$WERCKER_HELM_KUBECTL_TTY\""
-  fi
-
-  # grace-period
-  if [ -n "$WERCKER_HELM_KUBECTL_GRACE_PERIOD" ]; then
-    args="$args --grace-period=\"$WERCKER_HELM_KUBECTL_GRACE_PERIOD\""
-  fi
-
-  # selector
-  if [ -n "$WERCKER_HELM_KUBECTL_SELECTOR" ]; then
-    args="$args --selector=\"$WERCKER_HELM_KUBECTL_SELECTOR\""
-  fi
-
-  # all
-  if [ -n "$WERCKER_HELM_KUBECTL_ALL" ]; then
-    args="$args --all=\"$WERCKER_HELM_KUBECTL_ALL\""
-  fi
-
-  # overwrite
-  if [ -n "$WERCKER_HELM_KUBECTL_OVERWRITE" ]; then
-    args="$args --overwrite=\"$WERCKER_HELM_KUBECTL_OVERWRITE\""
-  fi
-  
-  if [ "$WERCKER_HELM_KUBECTL_DEBUG" = "true" ]; then
-    info "kubectl $global_args $raw_global_args $cmd $args $raw_args"
+  if [ -n "$WERCKER_HELM_CLIENT_KEY" ]; then
+    global_args="$global_args --client-key=\"$WERCKER_HELM_CLIENT_KEY\""
   fi
 
   mkdir -p $HOME/.kube
   $WERCKER_STEP_ROOT/envsubst < "$WERCKER_STEP_ROOT/config" > "$HOME/.kube/config"
-  cat $HOME/.kube/config
 
   # export KUBECONFIG= $HOME/.kube/config
   info "Connecting to the Cluster as specified in kubeconfig"
@@ -180,7 +72,7 @@ main() {
   info "Initializing Helm"
   "$helm" init --service-account tiller --kubeconfig $HOME/.kube/config
   info "Executing Helm Command"
-  eval "$helm" "$helm_cmd"
+  eval "$helm" "$helm_cmd" "$helm_args" 
   
 }
 
